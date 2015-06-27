c350dc5c9175e1085c8a6da6e5dd6b43297e1997
Aaron-Zhao123 <Aaron@ZhaodeMacBook-Pro.local>
2015-06-27 15:59:41 +0100

    fixed error, generation of samples work now

--- UROP research/Matlab/generate_samples.m
+++ UROP research/Matlab/generate_samples.m
@@ -5,12 +5,13 @@ function [positive,negative]=generate_samples(size)
     
 
     positive(size,8)=0;
-    negative(size,9)=0; %initial two vectors for memory allocation
+    negative(size,8)=0; %initial two vectors for memory allocation
     
-    digit=randi([0 20],size,1); %generates a 1*size array with numbers from 0 to 255(8 bits)
+    digit=randi([0 100],size,1); %generates a 1*size array with numbers from 0 to 255(8 bits)
     
     for i=1:size
-        cs_number=csdigit(digit(i));
+        cs_number=csdigit(digit(i),8);
+        
         for j=1:8
             if cs_number(j)=='+'
                 positive(i,j)=1;
